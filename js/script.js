7b0df63dc9039e4ea86817aa189ff220d533a25b
jimblu <jimmy.pezp@gmail.com>
2020-11-10 17:53:34 +0100

    update

--- js/script.js
+++ js/script.js
@@ -10,13 +10,19 @@ const educationBtn = document.querySelector('.educationBtn');
 const skillsBtn = document.querySelector('.skillsBtn');
 const interestsBtn = document.querySelector('.interestsBtn');
 
+const burger = document.querySelector('.burger');
+
 const scrollPage = (btn, element) => {
 btn.addEventListener('click', () => {
     window.scroll(0, element.offsetTop)
-})}
+})};
 
 scrollPage(aboutBtn, about);
 scrollPage(xpBtn, xp);
 scrollPage(educationBtn, education);
 scrollPage(skillsBtn, skills);
 scrollPage(interestsBtn, interests);
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+});
