05ea42cd8a8adc129129e3bf1ea7c98eb98cc536
jimblu <jimmy.pezp@gmail.com>
2020-11-10 12:22:58 +0100

    updates

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const about = document.querySelector('.about');
+const xp = document.querySelector('.experience');
+const education = document.querySelector('.education');
+const skills = document.querySelector('.skills');
+const interests = document.querySelector('.interests');
+
+const aboutBtn = document.querySelector('.aboutBtn');
+const xpBtn = document.querySelector('.xpBtn');
+const educationBtn = document.querySelector('.educationBtn');
+const skillsBtn = document.querySelector('.skillsBtn');
+const interestsBtn = document.querySelector('.interestsBtn');
+
+const scrollPage = (btn, element) => {
+btn.addEventListener('click', () => {
+    window.scroll(0, element.offsetTop)
+})}
+
+scrollPage(aboutBtn, about);
+scrollPage(xpBtn, xp);
+scrollPage(educationBtn, education);
+scrollPage(skillsBtn, skills);
+scrollPage(interestsBtn, interests);
